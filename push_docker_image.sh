8ba2071fc7a701e666f77e2a4281057482acbf45
Urook <uri@rookout.com>
2022-08-02 14:17:45 +0300

    Rename image

--- push_docker_image.sh
+++ push_docker_image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash -x
 
 VERSION="1.0.0"
-docker build --no-cache -t urook/message-sender:${VERSION} -t urook/message-sender:latest .
-docker push urook/message-sender:${VERSION}
-docker push urook/message-sender:latest
+docker build --no-cache -t rookout/message-sender:${VERSION} -t rookout/message-sender:latest .
+docker push rookout/message-sender:${VERSION}
+docker push rookout/message-sender:latest
