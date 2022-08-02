f3de3896d9499c4e54b5ad55a19a11d686a2745a
Urook <uri@rookout.com>
2022-08-02 14:17:06 +0300

    Use alpine, push image script and add certain git files

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-FROM node:16.16
+FROM node:16.16-alpine
 
 COPY package*.json ./
 RUN npm install
 COPY . .
+COPY .git /.git
 ENTRYPOINT npm start
---
+++ push_docker_image.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash -x
+
+VERSION="1.0.0"
+docker build --no-cache -t urook/message-sender:${VERSION} -t urook/message-sender:latest .
+docker push urook/message-sender:${VERSION}
+docker push urook/message-sender:latest
